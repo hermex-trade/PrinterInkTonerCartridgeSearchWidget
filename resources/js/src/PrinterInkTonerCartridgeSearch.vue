7a02ad62c2a12d5f438a79c153f51cee54316467
Nicolas Andrea Almy <nicolas.almy@gmail.com>
2021-07-11 14:49:08 +0200

    Initial commit

---
+++ resources/js/src/PrinterInkTonerCartridgeSearch.vue
@@ -0,0 +1,35 @@
+
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'PrinterInkTonerCartridgeSearch',
+    data () {
+        return {
+            level1Selection: null,
+            level1Options: null,
+            level2Selection: null,
+            level2Options: null,
+            level3Selection: null,
+            level3Options: null,
+            level4Selection: null,
+            level4Options: null,
+            level5Selection: null,
+            level5Options: null,
+            level6Selection: null,
+            level6Options: null,
+        }
+    },
+    computed: {
+        
+    },
+    watch: {
+
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
---
+++ src/Widgets/PrinterInkTonerCartridgeSearchWidget.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace PrinterInkTonerCartridgeSearchWidget\Widgets;
+
+use Ceres\Widgets\Helper\BaseWidget;
+use Ceres\Widgets\Helper\Factories\WidgetDataFactory;
+use Ceres\Widgets\Helper\Factories\WidgetSettingsFactory;
+use Ceres\Widgets\Helper\WidgetTypes;
+
+class PrinterInkTonerCartridgeSearchWidget extends BaseWidget
+{
+
+    protected $template = "PrinterInkTonerCartridgeSearchWidget::Widgets.PrinterInkTonerCartridgeSearchWidget";
+
+    protected function getTemplateData($widgetSettings, $isPreview)
+    {
+        $mainCategoryId = $widgetSettings["mainCategoryId"]["mobile"];
+
+        if (empty($mainCategoryId)) {
+            return [
+                "data" => false
+            ];
+        }
+
+        // If the main category id is set
+        // then we call the children on each subcategories recursively
+        if ($mainCategoryId) {
+            return [
+                "data" => [
+                    "mainCategoryId" => $mainCategoryId,
+                ]
+            ];
+        }
+
+        return [
+            "data" => false
+        ];
+    }
+
+    public function getData(): array
+    {
+        return WidgetDataFactory::make("PrinterInkTonerCartridgeSearch::PrinterInkTonerCartridgeSearchWidget")
+            ->withLabel("PrinterInkTonerCartridgeSearchWidget.printerInkTonerCartridgeSearchWidgetLabel")
+            ->withMaxPerPage(2)
+            ->withType(WidgetTypes::STATIC)
+            ->toArray();
+    }
+
+    public function getSettings(): array
+    {
+        $settings = pluginApp(WidgetSettingsFactory::class);
+        $settings->createNumber("mainCategoryId")
+            ->withName("PrinterInkTonerCartridgeSearchWidget.mainCategoryIdLabel")
+            ->withToolTip("PrinterInkTonerCartridgeSearchWidget.mainCategoryIdTooltip");
+        return $settings->toArray();
+    }
+
+}
+
